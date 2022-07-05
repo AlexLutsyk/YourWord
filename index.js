30981b47e7288d931429b3f461a7244ff371a2ca
Alex <ajlexmaritime@gmail.com>
2022-07-05 22:16:53 +0300

    small update

--- index.js
+++ index.js
@@ -11,15 +11,26 @@ const outputText = document.querySelector('.output-text');
 
 form.addEventListener('submit', onButtonWordClick);
 
+const localName = localStorage.getItem('name');
+const localWord = localStorage.getItem('word');
+if (localName) {
+  inputName.value = localName;
+}
+outputText.innerHTML = `${localName}, here is your word: ${localWord.toUpperCase()}`;
+searchWordBtn.textContent = `YOUR WORD - ${localWord.toUpperCase()}`;
+
 function onButtonWordClick(event) {
   event.preventDefault();
+
   const userName = event.currentTarget.name.value;
+  localStorage.setItem('name', userName);
 
   if (userName === '') {
     alert('Enter Your Name Please!');
   } else {
     getWord
       .then(word => {
+        localStorage.setItem('word', word[0]);
         outputText.innerHTML = `${userName}, here is your word: ${word[0].toUpperCase()}`;
         searchWordBtn.textContent = `YOUR WORD - ${word[0].toUpperCase()}`;
       })
